43fc2647232f410877b1b36b57601ddfd472059f
Chen Asraf <chen@asraf.me>
2016-03-29 13:41:19 +0300

    Asset refactoring

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, render_template
 from flask.ext import assets
+import glob
+import os
 
 app = Flask(__name__)
 
@@ -13,55 +15,53 @@ env.load_path = [
     os.path.join(os.path.dirname(__file__), 'bower_components'),
 ]
 
+bower_files = [
+    'jquery/dist/jquery.min.js',
+    'angularjs/angular.min.js',
+]
 
-env.register(
-    'js_sender',
-    assets.Bundle(
-        'jquery/dist/jquery.min.js',
-        'angularjs/angular.min.js',
-        assets.Bundle(
-            'sender.coffee',
-            filters=['coffeescript']
-        ),
-        output='js_sender.js'
-    )
-)
+receiver_js_files = glob.glob(os.path.join(
+    os.path.dirname(__file__), 'coffee', 'receiver', '*.coffee'))
+sender_js_files = glob.glob(os.path.join(
+    os.path.dirname(__file__), 'coffee', 'sender', '*.coffee'))
+receiver_css_files = glob.glob(os.path.join(
+    os.path.dirname(__file__), 'sass', 'receiver', '*.scss'))
+sender_css_files = glob.glob(os.path.join(
+    os.path.dirname(__file__), 'sass', 'sender', '*.scss'))
 
-env.register(
-    'css_sender',
-    assets.Bundle(
-        'sender.scss',
-        filters='scss',
-        output='css_sender.css'
-    )
-)
+receiver_js_files = map(lambda x: x[x.find(os.path.sep)+1:], receiver_js_files)
+sender_js_files = map(lambda x: x[x.find(os.path.sep)+1:], sender_js_files)
+receiver_css_files = map(lambda x: x[x.find(os.path.sep)+1:], receiver_css_files)
+sender_css_files = map(lambda x: x[x.find(os.path.sep)+1:], sender_css_files)
 
-env.register(
-    'js_receiver',
-    assets.Bundle(
-        'jquery/dist/jquery.min.js',
-        'angularjs/angular.min.js',
-        assets.Bundle(
-            'receiver.coffee',
-            filters=['coffeescript']
-        ),
-        output='js_receiver.js'
-    )
-)
+receiver_js_bundle = bower_files + \
+    [assets.Bundle(*receiver_js_files, filters='coffeescript',
+                   output='receiver.js')]
+sender_js_bundle = bower_files + \
+    [assets.Bundle(*sender_js_files, filters='coffeescript',
+                   output='sender.js')]
 
-env.register(
-    'css_receiver',
-    assets.Bundle(
-        'receiver.scss',
-        filters='scss',
-        output='css_receiver.css'
-    )
-)
+bundles = {
+    'receiver_js': assets.Bundle(*receiver_js_bundle),
+    'sender_js': assets.Bundle(*sender_js_bundle),
+    'receiver_css': assets.Bundle(*receiver_css_files,
+                                  filters='scss',
+                                  output='receiver.css'),
+    'sender_css': assets.Bundle(*sender_css_files,
+                                filters='scss',
+                                output='sender.css'),
+}
+
+print sender_css_files
+print sender_js_files
+
+env.register(bundles)
 
 
 @app.route("/")
 def index():
     return render_template('receiver.html')
+
 
 @app.route("/sender")
 def sender():
